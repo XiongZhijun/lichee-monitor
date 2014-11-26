c62fb31e1848e07e09177d9614f27ce1e8473934
XiongZhijun <hust.xzj@gmail.com>
2014-11-26 23:05:51 +0800

    refactor with spring boot

--- lichee-commons/src/main/java/org/upasx/lichee/zookeeper/LicheeZooKeeperFactoryBean.java
+++ lichee-commons/src/main/java/org/upasx/lichee/zookeeper/LicheeZooKeeperFactoryBean.java
@@ -5,19 +5,29 @@
 package org.upasx.lichee.zookeeper;
 
 import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
+import org.upasx.lichee.configs.AppProperties;
 
 /**
  * @author Xiong Zhijun
  * @date Nov 22, 2014
  *
  */
+@Lazy(false)
 @Component
 public class LicheeZooKeeperFactoryBean implements FactoryBean<LicheeZooKeeper> {
+	@Value("${zookeeper.servers}")
+	private String zooKeeperServers = AppProperties.INSTANCE
+			.getZooKeeperServers();
+	@Value("${zookeeper.session.timeout}")
+	private int zooKeeperSessionTimeout = AppProperties.INSTANCE
+			.getZooKeeperSessionTimeout();
 
 	@Override
 	public LicheeZooKeeper getObject() throws Exception {
-		return LicheeZooKeeperFactory.createZooKeeper();
+		return new LicheeZooKeeper(zooKeeperServers, zooKeeperSessionTimeout);
 	}
 
 	@Override
