505396907ca844eaad99bb7353f2fd755fd7be49
XiongZhijun <hust.xzj@gmail.com>
2014-12-14 21:19:04 +0800

    add restful controller

--- lichee-server/src/main/java/org/upasx/lichee/server/controller/HostsResource.java
+++ lichee-server/src/main/java/org/upasx/lichee/server/controller/HostsResource.java
@@ -8,8 +8,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.upasx.lichee.utils.PathUtils;
 import org.upasx.lichee.zookeeper.LicheeZooKeeper;
 
 /**
@@ -31,5 +33,28 @@ public class HostsResource {
 		List<String> children = this.licheeZooKeeper.getChildren(hostsPath,
 				null);
 		return children;
+	}
+
+	@RequestMapping("/hosts/{host}")
+	public List<String> host(@PathVariable("host") String host) {
+		List<String> children = this.licheeZooKeeper.getChildren(
+				PathUtils.join(hostsPath, host), null);
+		return children;
+	}
+
+	@RequestMapping("/hosts/{host}/{item}/datas")
+	public String hostDatas(@PathVariable("host") String host,
+			@PathVariable("item") String item) {
+		String data = this.licheeZooKeeper.getData(
+				PathUtils.join(hostsPath, host, item, "datas"), null);
+		return data;
+	}
+
+	@RequestMapping("/hosts/{host}/{item}/configs")
+	public String hostConfigs(@PathVariable("host") String host,
+			@PathVariable("item") String item) {
+		String data = this.licheeZooKeeper.getData(
+				PathUtils.join(hostsPath, host, item, "configs"), null);
+		return data;
 	}
 }
